60f340c928e759b6f8d804d34d86109e19761138
AndresTrabajo <Usuario@PROFESOR-CARPET>
2025-05-08 08:58:11 -0500

    quite boton

--- EF/Form1.Designer.cs
+++ EF/Form1.Designer.cs
@@ -44,7 +44,6 @@
             this.ayudaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.soporteTecnicoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.vercionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.button1 = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -168,21 +167,11 @@
             this.vercionToolStripMenuItem.Size = new System.Drawing.Size(157, 22);
             this.vercionToolStripMenuItem.Text = "Vercion";
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(120, 121);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(245, 113);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
@@ -212,7 +201,6 @@
         private System.Windows.Forms.ToolStripMenuItem registrarVentaToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem registrarEntradaYSalidaToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem revisarStockToolStripMenuItem;
-        private System.Windows.Forms.Button button1;
     }
 }
 
