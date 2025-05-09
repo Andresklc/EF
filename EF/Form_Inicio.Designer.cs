efeffa3fb4a4a855e2e9a2ba5427c05589543548
Luis Andres Larrea Cortez <N00446667@upn.pe>
2025-05-08 23:15:33 -0500

    Cambios en el formulario Inicio

--- EF/Form_Inicio.Designer.cs
+++ EF/Form_Inicio.Designer.cs
@@ -173,7 +173,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.84906F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(270, 195);
+            this.label1.Location = new System.Drawing.Point(276, 204);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(247, 47);
             this.label1.TabIndex = 1;
